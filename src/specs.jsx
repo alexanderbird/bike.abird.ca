d28766e02c08f8130de08441a428125282abdc5b
Alexander Bird <alex@alexanderbird.software>
2022-07-18 14:18:30 -0700

    Load maintenance log records into specs page

--- src/specs.jsx
+++ src/specs.jsx
@@ -1,6 +1,23 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import { h } from 'preact';
 import components from './components.json';
+import * as yaml from 'js-yaml';
+
+const maintenanceLog = yaml.load(fs.readFileSync(path.join(__dirname, '../maintenance_log.yaml'), 'utf-8'));
+
+const maintenanceLogByComponent = maintenanceLog
+  .reduce((grouped, entry) => {
+    grouped[entry.component] = grouped[entry.component] || [];
+    grouped[entry.component].push(entry);
+    return grouped;
+  }, {});
+
+const knownComponents = new Set(components.map(x => x.slug));
+const unknownComponents = Object.keys(maintenanceLogByComponent).filter(x => !knownComponents.has(x));
+if (unknownComponents.length) {
+  throw new Error(`Maintenance log contains unknown components: ${unknownComponents}`);
+}
 
 export const Specs = () => (
   <html>
@@ -16,6 +33,14 @@ export const Specs = () => (
             <div class='component-tile'>
               <h4>{ component.title }</h4>
               <img src={`/images/components/${component.slug}.svg`} />
+              <ul>
+                { (maintenanceLogByComponent[component.slug] || []).map(entry => (
+                  <li>
+                    <span>{entry.date.toISOString().slice(0, 10)}</span>
+                    <span>{entry.note}</span>
+                  </li>
+                ))}
+              </ul>
             </div>
           ))}
         </div>
