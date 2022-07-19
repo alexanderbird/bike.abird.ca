eb45f5d393e82d1d628e36bc86838c09e5c549fb
Alexander Bird <alex@alexanderbird.software>
2022-07-18 17:35:38 -0700

    Add initial bike build spec to maintenance log

--- src/specs.jsx
+++ src/specs.jsx
@@ -7,9 +7,11 @@ import * as yaml from 'js-yaml';
 const maintenanceLog = yaml.load(fs.readFileSync(path.join(__dirname, '../maintenance_log.yaml'), 'utf-8'));
 
 const maintenanceLogByComponent = maintenanceLog
-  .reduce((grouped, entry) => {
-    grouped[entry.component] = grouped[entry.component] || [];
-    grouped[entry.component].push(entry);
+  .reduce((grouped, day) => {
+    day.remarks.forEach(entry => {
+      grouped[entry.component] = grouped[entry.component] || [];
+      grouped[entry.component].push({ ...entry, date: day.date });
+    });
     return grouped;
   }, {});
 
@@ -27,17 +29,19 @@ export const Specs = () => (
     </head>
     <body>
       <article>
-        <h3><a href="https://ca.bikes.com/products/growler-40-21?variant=41078688776355">2021 Growler 40</a></h3>
+        <h3>2021 Growler 40</h3>
         <div class='component-tile-container'>
           { components.map(component => (
             <div class='component-tile'>
-              <h4>{ component.title }</h4>
-              <img src={`/images/components/${component.slug}.svg`} />
+              <h4>
+                <span>{ component.title }</span>
+                <img src={`/images/components/${component.slug}.svg`} />
+              </h4>
               <ul>
                 { (maintenanceLogByComponent[component.slug] || []).map(entry => (
                   <li>
-                    <span>{entry.date.toISOString().slice(0, 10)}</span>
-                    <span>{entry.note}</span>
+                    <span class='component-tile__item-date'>{entry.date.toISOString().slice(0, 10)}</span>
+                    <span>{entry.remark}</span>
                   </li>
                 ))}
               </ul>
