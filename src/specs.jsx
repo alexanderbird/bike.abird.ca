91ec78e7da5a3aedc743b4599ab53680db715ac9
Alexander Bird <alex@alexanderbird.software>
2022-07-18 20:21:39 -0700

    Update the specs font

--- src/specs.jsx
+++ src/specs.jsx
@@ -33,6 +33,9 @@ export const Specs = () => (
       <title>Specs</title>
       <link rel='stylesheet' href='css/specs.css' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='preconnect' href='https://fonts.googleapis.com'/>
+      <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin/>
+      <link href='https://fonts.googleapis.com/css2?family=Patua+One&display=swap' rel='stylesheet'/>
     </head>
     <body>
       <h1>2021 Growler 40</h1>
