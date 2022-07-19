f6c8396958c9f5b39d2ce3bd577d21a163f1bbed
Alexander Bird <alex@alexanderbird.software>
2022-07-18 19:08:49 -0700

    Add responsive meta tag

--- src/specs.jsx
+++ src/specs.jsx
@@ -32,6 +32,7 @@ export const Specs = () => (
     <head>
       <title>Specs</title>
       <link rel='stylesheet' href='css/specs.css' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
     </head>
     <body>
       <article>
